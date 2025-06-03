2717a00109e214f843474a3600d71bb0f7f0cfa8
jrp-stm <jmreyes@stm-aplicada.com>
2025-06-03 14:53:56 +0200

    First commit

---
+++ main.go
@@ -0,0 +1,148 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"log/slog"
+	"net/http"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+var log *slog.Logger
+
+var contacts_set []Contact
+var contacts_data []byte
+
+func main() {
+
+	//init logger
+	log = slog.New(slog.NewJSONHandler(os.Stdout, nil))
+
+	//loaf contacts
+	err := load_contacts()
+	if err != nil {
+		log.Error("Error in load_contacts()", "error", err)
+	}
+
+	//Define mux
+	mux := http.NewServeMux()
+
+	mux.Handle("GET /", http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		http.Redirect(w, r, "/contact", http.StatusFound)
+	}))
+	mux.Handle("GET /contact", http.HandlerFunc(contact_handler))
+	mux.Handle("GET /contact/{id}", http.HandlerFunc(contact_handler))
+
+	server := http.Server{
+		Addr:         ":8080",
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 90 * time.Second,
+		Handler:      logging(mux),
+	}
+	err = server.ListenAndServe()
+	if err != nil {
+		log.Error("Error in server.ListenAndServe", "error", err)
+		return
+	}
+
+}
+
+func logging(f http.Handler) http.Handler {
+	return (http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		id := uuid.New().String()
+		log := log.With("request_id", id)
+
+		log.Info("NewRequest",
+			"method", r.Method, "url", r.URL.Path,
+			"remoteAddress", r.RemoteAddr)
+
+		ctx := context.WithValue(r.Context(), "log", log)
+		r = r.WithContext(ctx)
+		//calls actual handler
+		f.ServeHTTP(w, r)
+	}))
+}
+
+func load_contacts() error {
+	var err error
+	contacts_data, err = os.ReadFile("contacts.json")
+	if err != nil {
+		return fmt.Errorf("contact_handler: error in osReadFile: %w", err)
+	}
+
+	err = json.Unmarshal(contacts_data, &contacts_set)
+	if err != nil {
+		return fmt.Errorf("contact_handler: error in json.Unmarhsall: %w", err)
+	}
+
+	return nil
+}
+
+type Contact struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Phone string `json:"phone"`
+}
+
+// /contacts/{id}
+func contact_handler(w http.ResponseWriter, r *http.Request) {
+
+	// Extract id from request query
+	id_string := r.PathValue("id")
+	if id_string == "" {
+		//Default response
+		w.Header().Set("Content-Type", "application/json")
+		_, err := w.Write(contacts_data)
+		if err != nil {
+			http.Error(w, "Error providing contacts", http.StatusInternalServerError)
+			log.Error("contact_handler: error in default w.Write()", "error", err)
+			return
+		}
+
+	} else {
+		//Parse id
+		id_int, err := strconv.Atoi(id_string)
+		if err != nil {
+			http.Error(w, "Invalid id", http.StatusBadRequest)
+			log.Info("contact_handler: could not parse id", "error", err)
+			return
+		}
+
+		//Search specific contact
+		var found = -1
+		for _, contact := range contacts_set {
+
+			if contact.ID == id_int {
+				found = id_int
+				contact_data, err := json.Marshal(contact)
+				if err != nil {
+					http.Error(w, "Error providing contact information", http.StatusInternalServerError)
+					log.Error("contact_handler: error in json.Marshall", "error", err)
+					return
+				}
+
+				// Show response
+				w.Header().Set("Content-Type", "application/json")
+				_, err = w.Write(contact_data)
+				if err != nil {
+					http.Error(w, "Error providing contact information", http.StatusInternalServerError)
+					log.Error("contact_handler: error in w.Write()", "error", err)
+					return
+				}
+				break
+			}
+		}
+
+		if found == -1 {
+			http.Error(w, "Error, contact not found in database", http.StatusNotFound)
+			log.Info("contact_handler: contact not found")
+			return
+		}
+	}
+}
